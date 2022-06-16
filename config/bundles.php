78b480bba8110e5118301f072cd6c444c8a3650f
Gwennael Jean <gwennael.jean@gmail.com>
2022-06-16 21:39:24 +0200

    feat(webpack): installation de symfony/webpack-encore-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
