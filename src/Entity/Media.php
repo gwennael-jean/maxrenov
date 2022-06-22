2bd4db5d0d336c92de1f14061ca952a2d8dd6781
Gwennael Jean <gwennael.jean@gmail.com>
2022-06-22 21:19:31 +0200

    feat(media): préparation de l'entité

---
+++ src/Entity/Media.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MediaRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+
+#[ORM\Entity(repositoryClass: MediaRepository::class)]
+class Media
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $path;
+
+    private ?File $binaryContent;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(string $path): self
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    public function getBinaryContent(): ?File
+    {
+        return $this->binaryContent;
+    }
+
+    public function setBinaryContent(?File $binaryContent): Media
+    {
+        $this->binaryContent = $binaryContent;
+        return $this;
+    }
+}
