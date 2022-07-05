e7d2d82fc5af8057845c0bc72d53005de53022c2
Gwennael Jean <gwennael.jean@gmail.com>
2022-07-05 22:48:47 +0200

    feat(google review): préparation du composant

---
+++ src/Twig/Component/GoogleReviewComponent.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Twig\Component;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent('google-review', template: '.components/google-review.html.twig')]
+class GoogleReviewComponent
+{
+
+}
