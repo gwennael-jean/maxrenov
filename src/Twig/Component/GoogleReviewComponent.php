40d109a27d2d81b1027d9e7f909426ae56b8e7f8
Gwennael Jean <gwennael.jean@gmail.com>
2022-07-08 10:46:38 +0200

    feat(google review): récupération des avis depuis le composant GoogleReviewComponent

--- src/Twig/Component/GoogleReviewComponent.php
+++ src/Twig/Component/GoogleReviewComponent.php
@@ -2,10 +2,17 @@
 
 namespace App\Twig\Component;
 
+use App\Service\GoogleReview\GoogleReviewProvider;
+use App\Service\GoogleReview\Model\GoogleReview;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 #[AsTwigComponent('google-review', template: '.components/google-review.html.twig')]
 class GoogleReviewComponent
 {
+    public GoogleReview $googleReview;
 
+    public function __construct(GoogleReviewProvider $googleReviewProvider)
+    {
+        $this->googleReview = $googleReviewProvider->getData();
+    }
 }
