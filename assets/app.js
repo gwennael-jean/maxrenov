e7c780c2d294a1d8661f256714117f0f546326db
Gwennael Jean <gwennael.jean@gmail.com>
2022-07-12 14:28:51 +0200

    feat(topbar): création d'une topbar

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,7 @@
 // any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss';
 
-import 'jquery';
+import * as $ from 'jquery';
 import 'bootstrap/dist/js/bootstrap';
 
 // start the Stimulus application
