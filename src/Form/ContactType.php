c5e6e16dab0a684288d486dbdfd7ce7fa567d9a9
Gwennael Jean <gwennael.jean@gmail.com>
2022-07-18 22:21:44 +0200

    feat(contact): validation du formulaire de contact

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -22,10 +24,10 @@ class ContactType extends AbstractType
             ->add('lastname', TextType::class, [
                 'required' => false,
             ])
-            ->add('email', TextType::class, [
+            ->add('email', EmailType::class, [
                 'required' => true,
             ])
-            ->add('phone', TextType::class, [
+            ->add('phone', TelType::class, [
                 'required' => false,
             ])
             ->add('message', TextareaType::class, [
